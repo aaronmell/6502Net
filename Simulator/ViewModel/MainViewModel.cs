25a55e5191c85ce2dbf0ece2458552be5f17fa7c
Aaron Mell <aaronamell@gmail.com>
2013-04-12 08:20:28 -0500

    Basic design of Simulator Completed
    Nothing is wired up yet, and it looks ugly

---
+++ Simulator/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+using GalaSoft.MvvmLight;
+
+namespace Simulator.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+		/// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+        }
+    }
+}
