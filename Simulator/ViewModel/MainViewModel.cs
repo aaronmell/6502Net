e1fd60d5e58f185a1a157a778f5b0ecacb940038
Aaron Mell <aaronamell@gmail.com>
2013-04-12 08:23:16 -0500

    Resharper Cleanup
    Cleaned up the simulator

--- Simulator/ViewModel/MainViewModel.cs
+++ Simulator/ViewModel/MainViewModel.cs
@@ -16,11 +16,5 @@ namespace Simulator.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-		/// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        public MainViewModel()
-        {
-        }
-    }
+	}
 }
--- Simulator/ViewModel/ViewModelLocator.cs
+++ Simulator/ViewModel/ViewModelLocator.cs
@@ -12,7 +12,6 @@
   See http://www.galasoft.ch/mvvm
 */
 
-using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 
